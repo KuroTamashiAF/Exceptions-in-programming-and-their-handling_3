1a06879f668d96a56cacf4b8b51ae183d29135fe
Fedorov <chugun777akk@mail.ru>
2022-12-03 15:52:33 +0700

    problem with gender constructor in personService

---
+++ src/Service/Parser.java
@@ -0,0 +1,8 @@
+package Service;
+
+public class Parser {
+public String[] parse(String data){
+    String []arr  = data.split(" ");
+    return arr;
+    }
+}
--- src/Service/PersonServiceImpl.java
+++ src/Service/PersonServiceImpl.java
@@ -1,6 +1,9 @@
 package Service;
 
+import ObjectClases.Enum.Gender;
 import ObjectClases.Person;
+
+import java.util.Scanner;
 
 public class PersonServiceImpl implements PersonService{
     Person person;
@@ -9,6 +12,12 @@ public class PersonServiceImpl implements PersonService{
     public void personAdd(String date) {
         System.out.println("Введите данные согласно формату (Имя Фамилия Отчество Дата рождения Номер телефона Пол) " +
                 "через пробел ");
+        Scanner sc = new Scanner(System.in);
+        String goToParse  = sc.nextLine();
+        Parser prs = new Parser();
+        String [] arr = prs.parse(goToParse);
+        new Person(arr[0], arr[1], arr[2], arr[3], Integer.parseInt(arr[4]),arr[5]);
+
 
     }
 }

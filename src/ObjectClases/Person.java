8b6b8285552bbf85a1e2dd92f62c5b128cd4a181
Fedorov <chugun777akk@mail.ru>
2022-12-03 15:06:10 +0700

    add constructor add Getter in class Person

--- src/ObjectClases/Person.java
+++ src/ObjectClases/Person.java
@@ -1,13 +1,47 @@
 package ObjectClases;
 
+import ObjectClases.Gender.Gender;
+
+import java.util.Scanner;
+
 public class Person {
     private String surname;
     private String name;
     private String middleName;
     private String dateOfBirth;  // dd.mm.yyyy
     private int numberOfPhone;
+    private Gender gender;
 
+    public Person(String surname, String name, String middleName, String dateOfBirth, int numberOfPhone, Gender gender) {
+        this.surname = surname;
+        this.name = name;
+        this.middleName = middleName;
+        this.dateOfBirth = dateOfBirth;
+        this.numberOfPhone = numberOfPhone;
+        this.gender = gender;
+    }
 
+    public String getSurname() {
+        return surname;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getNumberOfPhone() {
+        return numberOfPhone;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
 }

632e159c2c1db5c218987fd6bd5ffedc792c14bc
Fedorov <chugun777akk@mail.ru>
2022-12-03 15:16:17 +0700

    add method addPerson in repository

--- src/Repository/PersonRepository.java
+++ src/Repository/PersonRepository.java
@@ -1,4 +1,22 @@
 package Repository;
 
+import ObjectClases.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonRepository {
+    private List <Person> arrPerson;
+
+    public PersonRepository() {
+        this.arrPerson = new ArrayList<>();
+    }
+    public void addPersonInBase(Person person)
+    {
+        this.arrPerson.add(person);
+    }
+
+    public List<Person> getArrPerson() {
+        return this.arrPerson;
+    }
 }

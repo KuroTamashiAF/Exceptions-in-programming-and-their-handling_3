7abc87a3f0907b91f987eb2f7ee70af6a5ee08f4
Fedorov <chugun777akk@mail.ru>
2022-12-04 15:13:39 +0700

    problem is done

--- src/Service/PersonServiceImpl.java
+++ src/Service/PersonServiceImpl.java
@@ -24,9 +24,14 @@ public class PersonServiceImpl implements PersonService{
         System.out.println("Введите данные согласно формату (Фамилия Имя Отчество Дата рождения Номер телефона Пол) " +
                 "через пробел ");
         Scanner sc = new Scanner(System.in);
-        String goToParse  = sc.nextLine();
-        Parser prs = new Parser();
-        String [] arr = prs.parse(goToParse);
+            String goToParse = sc.nextLine();
+            Parser prs = new Parser();
+            String[] arr = prs.parse(goToParse);
+
+                if (arr.length != 6) {
+                    throw new RuntimeException("Введите все данные через 1 пробел ");
+                }
+
         this.personSer = new Person(arr[0], arr[1], arr[2],arr[3], Long.parseLong(arr[4]), arr[5].charAt(0));
 
 

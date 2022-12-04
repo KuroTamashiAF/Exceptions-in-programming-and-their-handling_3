e7f415f79eb2eef2806f7490931b4a1a7206d531
Fedorov <chugun777akk@mail.ru>
2022-12-04 15:48:57 +0700

    add try catch in Person Service

--- src/Service/PersonServiceImpl.java
+++ src/Service/PersonServiceImpl.java
@@ -24,16 +24,23 @@ public class PersonServiceImpl implements PersonService{
         System.out.println("Введите данные согласно формату (Фамилия Имя Отчество Дата рождения Номер телефона Пол) " +
                 "через пробел ");
         Scanner sc = new Scanner(System.in);
+        while(true) {
             String goToParse = sc.nextLine();
             Parser prs = new Parser();
             String[] arr = prs.parse(goToParse);
-
+            try {
                 if (arr.length != 6) {
-                    throw new RuntimeException("Введите все данные через 1 пробел ");
+                    throw new RuntimeException();
+                } else {
+                    this.personSer = new Person(arr[0], arr[1], arr[2], arr[3], Long.parseLong(arr[4]),
+                            arr[5].charAt(0));
+                    break;
                 }
-
-        this.personSer = new Person(arr[0], arr[1], arr[2],arr[3], Long.parseLong(arr[4]), arr[5].charAt(0));
-
+            }catch (RuntimeException e){
+                System.out.println("Вы ввели слишком мало или слишком много данных \n" +
+                        "Повторите ввод");
+            }
+        }
 
     }
 
